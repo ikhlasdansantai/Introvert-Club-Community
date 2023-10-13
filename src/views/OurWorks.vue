8db62711b82a636ea1419daa4364eb2f47a990bc
ikhlasdansantai <initesting5@gmail.com>
2023-10-13 19:39:30 +0700

    menambahkan karya baru (YT Video)

--- src/views/OurWorks.vue
+++ src/views/OurWorks.vue
@@ -12,7 +12,15 @@
       <h2 class="font-semibold text-xl mt-10 md:mt-20">Karya Instagram</h2>
       <div class="websites-contents grid gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-4">
         <div class="our-works-card" v-for="(instagramData, index) in instagram" :key="index">
-          <ourWorksCard :title="instagramData.title" :imgContent="instagramData.imgContent" :imgAuthor="instagramData.imgAuthor" :authorName="instagramData.authorName" :iconType="instagramData.icon" :linkPath="instagram.link" />
+          <ourWorksCard :title="instagramData.title" :imgContent="instagramData.imgContent" :imgAuthor="instagramData.imgAuthor" :authorName="instagramData.authorName" :iconType="instagramData.icon" :linkPath="instagramData.link" />
+        </div>
+      </div>
+    </div>
+    <div class="instagram mt-20">
+      <h2 class="font-semibold text-xl mt-10 md:mt-20">Karya Musik Video</h2>
+      <div class="websites-contents grid gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-4">
+        <div class="our-works-card" v-for="(youtubeData, index) in youtube" :key="index">
+          <ourWorksCard :title="youtubeData.title" :imgContent="youtubeData.imgContent" :imgAuthor="youtubeData.imgAuthor" :authorName="youtubeData.authorName" :iconType="youtubeData.icon" :linkPath="youtubeData.link" />
         </div>
       </div>
     </div>
@@ -25,6 +33,10 @@ import RenzMakeup from "../assets/our-works-images/Website/RenzMakeup.png";
 import imgAuthor from "../assets/Ayang.jpg";
 import MyProgressIG from "../assets/our-works-images/Instagram/MyProgress.png";
 import RenzAuraIG from "../assets/our-works-images/Instagram/RenzAura.png";
+
+import cmjtvYT from "../assets/our-works-images/VideoYT/cmjtv.png";
+import cmjtvAuthor from "../assets/author/cmjtv.jpeg";
+
 import ourWorksCard from "../components/cards/ourWorksCard.vue";
 export default {
   components: { ourWorksCard },
@@ -72,6 +84,16 @@ export default {
           authorName: "Ikhlasdansantai",
           icon: "mdi:instagram",
           link: "https://www.instagram.com/renz_aura/",
+        },
+      ],
+      youtube: [
+        {
+          imgContent: cmjtvYT,
+          imgAuthor: cmjtvAuthor,
+          title: "Asep Aziz - Untuk Sejenak",
+          authorName: "Asep Aziz",
+          icon: "mdi:youtube",
+          link: "https://www.youtube.com/watch?v=QQXzEkuHyns",
         },
       ],
     };
